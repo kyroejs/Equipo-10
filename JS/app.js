9f1c2d6bb849a5fc7f5096abda19b0e44bed3e22
LennyPedraza <lennyinnelcocompedraza@gmail.com>
2023-06-16 11:28:14 -0600

    estilos en la ventana de exito

--- JS/app.js
+++ JS/app.js
@@ -2,12 +2,19 @@ const button = document.getElementById("boton-registro");
 const div = document.getElementById("mensaje-registro");
 
 
-button.addEventListener("click", function(event) {
-  event.preventDefault();
-  div.style.display = "block";
-setTimeout(function() {
-    div.style.display = "none";
-    document.getElementById("registro").reset();
-  }, 1000);
+button.addEventListener("click", function (event) {
+    event.preventDefault();
+    div.style.display = "block";
+    setTimeout(function () {
+        div.style.display = "none";
+        document.getElementById("registro").reset();
+    }, 1000);
 
+});
+var butn = document.getElementById("myButton");
+
+button.addEventListener("click", function(event) {
+  event.preventDefault(); // Evita la recarga de la página
+  // Tu código aquí
+  console.log('El botón ha sido presionado');
 });
